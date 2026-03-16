1054d7fd6d1eb39751a8f0216d56ecb4e285125b
Thomas Auinger <thomas@auinger.de>
2026-03-16 13:44:38 +0100

    sltn: added datastructures module

--- testing/src/main/java/de/codevibe/library/Library.java
+++ testing/src/main/java/de/codevibe/library/Library.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class Library {
 
     private final Map<String, Book> books = new HashMap<>();
-
     private final Set<String> borrowedIsbns = new HashSet<>();
+    long priceQueryCount = 0;
 
     public void addBook(Book book) {
         bookExists(book);
@@ -54,11 +54,37 @@ public class Library {
 
     /**
      * Checks if a book with the given ISBN does exist in this library.
+     *
      * @throws BookNotAvailableException if it doesn't exist
      */
     private void checkBookExists(String isbn) {
         if (!books.containsKey(isbn)) {
             throw new BookNotAvailableException("Sorry, book with ISBN " + isbn + " doesn't exist");
+        }
+    }
+
+
+    /**
+     * Returns the default price of 2.50, but if younger than 18 years only 1.00 €. Students benefit from a discount of 50 cnt.
+     *
+     * @return
+     */
+    public float getBorrowPrice(int age, boolean isStudent) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Age is less than 0");
+        }
+
+        // for internal tracking, count number of queries
+        priceQueryCount++;
+
+        // business logic:
+        final float defaultPrice = 2.50f;
+        if (age < 18) {
+            return 1.00f;
+        } else if (isStudent) {
+            return defaultPrice - 0.50f;
+        } else {
+            return defaultPrice;
         }
     }
 }
--- testing/src/test/java/de/codevibe/library/LibraryTest.java
+++ testing/src/test/java/de/codevibe/library/LibraryTest.java
@@ -1,63 +1,143 @@
-package test.java.de.codevibe.library;
+<<<<<<<< HEAD:testing/src/test/java/de/codevibe/LibraryTest.java
+package test.java.de.codevibe;
 
-import main.java.de.codevibe.library.Library;
 import main.java.de.codevibe.library.Book;
-import org.junit.jupiter.api.Test;
+import main.java.de.codevibe.library.BookNotAvailableException;
+import main.java.de.codevibe.library.Library;
+========
+package de.codevibe.library;
+
+>>>>>>>> 7142c37 (sltn: added datastructures module):testing/src/test/java/de/codevibe/library/LibraryTest.java
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LibraryTest {
-    private Library library = new Library();
-    @Test
-    void addBook() {
-        Book book = new Book("12345", "Buy High, Sell Low.", "Rainer Winkler");
-        library.addBook(book);
-        for (Book book1 : library.getAllBooks()) {
-            if (book.getIsbn().equals(book1.getIsbn())) {
-                assertEquals(book.getIsbn(), book1.getIsbn());
-            }
-        }
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class LibraryTest {
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @BeforeEach
+    void setUp() {
+        book1 = new Book("123", "haha", "JRR T.");
+        book2 = new Book("456", "hehe", "JRR T.");
+        book3 = new Book("789", "hihi", "JRR T.");
+        library = new Library();
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
     }
 
-    @Test
-    void borrowBook() {
-        Book book = new Book("12345", "Buy High, Sell Low.", "Rainer Winkler");
-        library.addBook(book);
-        library.borrowBook("12345");
-        assertFalse(library.isAvailable("12345"));
-        assertTrue(Exception,library.addBook(null));
-    }
 
     @Test
-    void returnBook() {
-        library.returnBook("12345");
-        assertTrue(library.isAvailable("12345"));
-        assertFalse(library.isAvailable("13579"));
-
-    }
-
-    @Test
-    void isAvailable() {
-        assertTrue(library.isAvailable("12345"));
-    }
-
-    @Test
+    @Order(1)
     void getBookCount() {
-        library.addBook(new Book("54321", "Do Yappies Dream Of The Big Short?", "Rainer Zufall"));
-        assertEquals(1, library.getBookCount());
+        assertEquals(3, library.getBookCount());
     }
 
     @Test
+    @Order(2)
+    void addBook() {
+        library.addBook(new Book("999", "haha", "JRR T."));
+        assertEquals(4, library.getBookCount());
+    }
+
+
+    @Test
+    @Order(3)
+    void isAvailable__succeeds() {
+        assertTrue(library.isAvailable("123"));
+        assertTrue(library.isAvailable("456"));
+        assertTrue(library.isAvailable("789"));
+    }
+
+    @Test
+    @Order(3)
+    void isAvailable__failsForMissingBook() {
+        try {
+            library.isAvailable("111");
+            Assertions.fail("We should have raised an exception");
+        }
+        catch (BookNotAvailableException e) {
+            // empty by design -- we expect an exception to be thrown
+        }
+    }
+
+
+    @Test
+    @Order(4)
     void getBorrowedCount() {
         assertEquals(0, library.getBorrowedCount());
     }
 
+
+
     @Test
-    void getAllBooks() {
-        library.addBook(new Book("67889", "Big Fish, Small Fish", "Spandax Dad"));
-        library.addBook(new Book("78905", "Where Is My Money!?", "Spandax Dad"));
-        int size = library.getAllBooks().size();
-        assertEquals(2, size);
+    @Order(5)
+    void borrowBook() {
+        library.borrowBook("123");
+        assertEquals(1, library.getBorrowedCount());
+        library.borrowBook("456");
+        assertEquals(2, library.getBorrowedCount());
     }
+
+    @Test
+    @Order(6)
+    void borrowBooknotAvailable() {
+        library.borrowBook("123");
+        library.borrowBook("456");
+        assertEquals(2, library.getBorrowedCount());
+        library.borrowBook("123");
+        assertEquals(2, library.getBorrowedCount());
+    }
+
+    @Test
+    @Order(7)
+    void returnBook() {
+        library.borrowBook("123");
+        assertEquals(1, library.getBorrowedCount());
+        library.returnBook("123");
+        assertEquals(0, library.getBorrowedCount());
+    }
+
+    @Test
+    @Order(8)
+    void returnBookNotBorrowed() {
+        library.returnBook("123");
+        assertEquals(0, library.getBorrowedCount());
+    }
+
+    @Test
+    @Order(9)
+    void returnBooknotFromLibrary() {
+        library.returnBook("999");
+        assertEquals(0, library.getBorrowedCount());
+    }
+
+
+    @Test
+    @Order(10)
+    void addBook__failsSinceBookAlreadyExists() {
+        library.addBook(book1);
+        assertEquals(4, library.getBookCount());
+    }
+
+
+    @Test
+    void getBorrowPrice__blackBox() {
+        float actual = library.getBorrowPrice(18, false);
+        assertEquals(1.00f, actual);
+    }
+
+    @Test
+    void getBorrowPrice__queryCountWorks() {
+        long countBefore = library.priceQueryCount;
+        library.getBorrowPrice(18, true);
+        long countAfter = library.priceQueryCount;
+        assertEquals(countBefore + 1, countAfter);
+    }
+
 }
