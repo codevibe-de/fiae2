939be779c6d7415c3199f3651c3991372def4c22
Thomas Auinger <thomas@auinger.de>
2026-03-16 15:27:42 +0100

    Pair

---
+++ datastructures/src/main/java/de/codevibe/data/Pair.java
@@ -0,0 +1,21 @@
+package de.codevibe.data;
+
+public class Pair<T> {
+
+    private final T left;
+    private final T right;
+
+    public Pair(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getLeft() {
+        return left;
+    }
+
+    public T getRight() {
+        return right;
+    }
+
+}
