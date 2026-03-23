7bd3818b7ad75a6077bdaed23a8374221f18a955
Thomas Auinger <thomas@auinger.de>
2026-03-23 15:10:27 +0100

    emoji

--- io/src/main/java/de/codevibe/io/IoApp.java
+++ io/src/main/java/de/codevibe/io/IoApp.java
@@ -1,29 +1,26 @@
 package de.codevibe.io;
 
-import org.apache.commons.io.IOUtils;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class IoApp {
 
     public static void main(String[] args) {
+        String emoji = "A😊B";
+        System.out.println(emoji.length());
+
         try {
             InputStream inStream = new FileInputStream("blah.txt");
             byte[] bytes = new byte[1000];
             inStream.read(bytes);
             String s = new String(bytes, StandardCharsets.UTF_8);
             System.out.println(s);
-        }
-        catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("Uh oh, nicht gefunden: " + e.getMessage());
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("Uh oh, Fehler beim Lesen: " + e.getMessage());
         }
     }
