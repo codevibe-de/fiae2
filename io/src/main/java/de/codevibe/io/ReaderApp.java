7439da3de911394140cb1cb28aed69771d2fc474
mar <saturn2005gaming@gmail.com>
2026-03-30 10:34:37 +0200

    writer reader test

--- io/src/main/java/de/codevibe/io/ReaderApp.java
+++ io/src/main/java/de/codevibe/io/ReaderApp.java
@@ -1,15 +1,29 @@
 package de.codevibe.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
 public class ReaderApp {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("blah.txt", StandardCharsets.UTF_8, true))) {
+            bw.write("Test");
+            bw.newLine();
+            bw.write("asd");
+            bw.flush();
+        }
 
-    public static void main(String[] args) throws IOException {
-        Reader reader = new FileReader("blah.txt", StandardCharsets.UTF_8);
+        System.out.println(System.getProperty("user.dir") + "\\" + "blah.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader("blah.txt", StandardCharsets.UTF_8))) {
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
-
 }
